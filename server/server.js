336655c24a20ba4200dbb954a750e3289ee2c19c
Jonan <jjsalomonzon@gmail.com>
2020-04-03 23:22:05 -0400

    mlab test

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const socketIO = require('socket.io');
 const publicPath = path.join(__dirname,'/../public');
 const port = process.env.PORT || 3000;
 
+const mongo = require('mongodb').MongoClient;
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+
+
 let app = express()
 let server = http.createServer(app)
 let io = socketIO(server)
@@ -18,4 +22,30 @@ server.listen(port, () => {
 
 io.on('connect', (socket) => {
   console.log('Connected')
+
+  var data = {
+    hello: 'world'
+  }
+  saveToDatabase(data, () => {
+    io.to(socket.id).emit('messageSent', 'Results saved to database.');
+  });
 });
+
+function saveToDatabase(data, callback){
+
+  mongo.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }, (err, client) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const db = client.db('heroku_f2wxxsgg');
+    const collection = db.collection('data');
+
+    collection.insertOne(results, (err, result) => {});
+
+    callback();
+  });
+}
